66fccc6d9f24b8369980fee16de24caa13d7d57e
안남규 <mkdir.nkahn@gmail.com>
2023-05-30 19:17:54 +0900

    Add : likes

--- foodie_road/users/urls.py
+++ foodie_road/users/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from users.views import SignupView, SigninView, EmailUniqueCheckView, UserIdCheck
+from users.views import SignupView, SigninView, EmailUniqueCheckView, LikeView
 
 urlpatterns = [
     path('/signup', SignupView.as_view()),
     path('/email_check', EmailUniqueCheckView.as_view()),
     path('/signin', SigninView.as_view()),
-    path('/check', UserIdCheck.as_view()),
+    path('/like', LikeView.as_view()),
 ]
